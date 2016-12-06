490a19ba02db5f452f7e04e5e2bd948e9781fb8e
heoyunjee <heoyunjee@gmail.com>
2016-12-07 02:46:41 +0900

    Add main page using gulp, webpack for testing

---
+++ src/js/main-page.js
@@ -0,0 +1,134 @@
+var header_carousel = {
+  'width': 1640,
+  'height': {
+    'desktop': 850,
+    'tablet': 650,
+    'mobile': 600
+  }
+};
+var main_box_office_carousel = {
+  'height': {
+    'desktop': 530,
+    'tablet': 530
+  },
+  'index': {
+    'desktop': 2,
+    'tablet': 1,
+    'mobile': 0
+  },
+  'col': {
+    'desktop': 5,
+    'tablet': 3,
+    'mobile': 1
+  },
+  'margin': 20,
+  'infinite': {
+    'desktop': true,
+    'tablet': true,
+    'mobile': false
+  }
+};
+var main_movie_ranking_carousel = {
+  'height': {
+    'desktop': 690,
+    'tablet': 690
+  },
+  'count': {
+    'desktop': 5,
+    'tablet': 3,
+    'mobile': 1
+  },
+  'col': {
+    'desktop': 5,
+    'tablet': 3,
+    'mobile': 1
+  },
+  'row': 2,
+  'margin': 20,
+};
+
+var main_movie_trailers_carousel = {
+  'height': 225,
+  'count': {
+    'desktop': 3,
+    'tablet': 2,
+    'mobile': 1
+  },
+  'col': 3,
+  'margin': 20
+};
+
+
+
+
+var $caro4 = $('#main-header-carousel').fingoCarousel(header_carousel);
+var $caro = $('#main-box-office-carousel').fingoCarousel(main_box_office_carousel);
+var $caro2 = $('#main-movie-ranking-carousel').fingoCarousel(main_movie_ranking_carousel);
+var $caro3 = $('#main-new-movie-trailers-carousel').fingoCarousel(main_movie_trailers_carousel);
+// var $ca = $('#carousel').fingoCarousel({});
+
+
+  var main_movie_ranking_btn_group = document.querySelector('.main-movie-ranking-btn-group');
+  var active_btns = main_movie_ranking_btn_group.querySelectorAll('button');
+
+  active_btns.forEach(function(btn, index) {
+    btn.addEventListener('click', function() {
+      $(btn).radioClass('active');
+    });
+  });
+
+
+
+
+  var tabs = document.querySelectorAll('.ui-carousel-tab');
+
+  // 인디케이터 기본 이벤트 차단
+  tabs.forEach(function(tab) {
+    tab.addEventListener('click', function(e) {
+      if(e) { e.preventDefault(); }
+    });
+  });
+
+  var box_office_list = document.querySelector('.main-box-office-list');
+  var box_office_list_btn = box_office_list.querySelector('button');
+  var ul = box_office_list.querySelector('ul');
+
+  var index = 1;
+  var intervalID = null;
+  var $ul = $(ul);
+
+
+  // Box Office List 버튼 이벤트 처리
+  box_office_list_btn.addEventListener('click', function() {
+    box_office_list.classList.toggle('show');
+    if(box_office_list.classList.contains('show')) {
+      $ul.css('top', 0);
+      clearInterval(intervalID);
+      for(var i = 0, l = index - 1; i < l; i++) {
+        $ul.prepend($ul.children().last());
+      }
+    } else if(!box_office_list.classList.contains('show')) {
+      index = 1;
+      intervalID = window.setInterval(rollingList, 2500);
+    }
+  });
+
+  // Box Office List 롤링 함수
+  function rollingList() {
+    var list_length = $ul.children().length;
+
+    $ul.animate({
+      'top': -45
+    }, 600, function() {
+      $(this).append($(this).children().first());
+      $(this).css('top', 0);
+      index++;
+    });
+  }
+
+  // Box Office List 롤링 수행
+  if(!box_office_list.classList.contains('show')) {
+    intervalID = window.setInterval(rollingList, 2500);
+  }
+
+// })(this, this.jQuery);
---
+++ src/js/main.es6
@@ -0,0 +1,16 @@
+/*! main.js © yamoo9.net, 2016 */
+
+let fingo = angular.module('FingoApp', []);
+
+// ListController
+require('./controllers/ListController');
+//
+// // Custom Filter
+// require('./filters/readingZeroFilter')
+//
+// require('./lib/jquery.min');
+require('./lib/jquery.easing.min');
+require('./jquery.radioClass');
+require('./ui-carousel-refactoring');
+require('./main-page');
+require('./main-add-comment');
