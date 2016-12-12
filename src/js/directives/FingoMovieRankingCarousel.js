b871d27a6a7a77812a6cd7227a96204e800c4f9c
heoyunjee <heoyunjee@gmail.com>
2016-12-12 15:35:50 +0900

    Add comment dim

--- src/js/directives/FingoMovieRankingCarousel.js
+++ src/js/directives/FingoMovieRankingCarousel.js
@@ -37,12 +37,22 @@ angular
           var comment_dim_el = document.querySelector('.comment-dim-container');
           var dim_1 = comment_dim_el.querySelector('.dim');
           var dim_2 = comment_dim_el.querySelector('.dim2');
+          var modal_rank_contents = comment_dim_el.querySelector('.modal-rank-contents');
 
+          var modal_rank_btn = comment_dim_el.querySelector('.modal-rank-btn');
           var modal_close_btn = comment_dim_el.querySelector('.modal-close-button');
-          var modal_ok_button = document.querySelector('.modal-ok-button');
+          var modal_ok_button = comment_dim_el.querySelector('.modal-ok-button');
+
+          var modal_rank_contents_left = 0;
 
           dim_1.addEventListener('click', hideDimModal);
           dim_2.addEventListener('click', hideDimModal);
+
+          modal_rank_btn.addEventListener('click', function() {
+            dim_2.style.display = 'block';
+            modal_rank_contents.style.display = 'block';
+          });
+
 
           modal_close_btn.addEventListener('click', function() {
             comment_dim_el.style.display = 'none';
@@ -57,14 +67,18 @@ angular
 
           function showDimModal() {
             var modals = document.querySelectorAll('.modal');
-            var dim = document.querySelector('.dim');
-            var dim_2 = document.querySelector('.dim2');
+            dim = document.querySelector('.dim');
+            dim_2 = document.querySelector('.dim2');
 
             dim.style.display = 'block';
             dim_2.style.display = 'block';
             for(var i = 0, l = modals.length; i < l; i++) {
               modals[i].style.display = 'block';
             }
+            console.log(modal_rank_contents);
+            console.log(modal_rank_btn.offsetLeft);
+            modal_rank_contents.style.left = (modal_rank_btn.offsetLeft - 127) + 'px';
+            console.log((modal_rank_btn.offsetLeft - (modal_rank_contents.offsetWidth / 2)));
           };
 
           function hideDimModal(){
@@ -90,6 +104,7 @@ angular
                 btn.addEventListener('click', function(e) {
                   if(e) { e.preventDefault(); e.stopPropagation();}
                   console.log('click');
+
                   var title = this.parentNode.querySelector('h3').textContent;
                   var poster_src = this.parentNode.parentNode.querySelector('img').getAttribute('src');
 
