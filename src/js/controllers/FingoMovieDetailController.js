a0743698f37d9ae86ca96adfa23cc616ae6601cc
DESKTOP-JAOS7MI\Joeun <joeun2312@gmail.com>
2016-12-16 06:57:22 +0900

    Add infinite-scroll

--- src/js/controllers/FingoMovieDetailController.js
+++ src/js/controllers/FingoMovieDetailController.js
@@ -10,55 +10,61 @@ angular.module('FingoApp').controller('FingoMovieDetailController', ['$scope', '
   $scope.selected_movie = null;
   $scope.popup_show_index = null;
   $scope.popup_show_active = false;
+  $scope.length_item = 3;
   var dataObject = 1;
+  $scope.length_num = function(num){
+    $scope.length_item += num;
+  }
   $scope.popup_show = function(movie, id, boolean){
     console.log(id);
     console.log(boolean);
     $scope.movie_id = id;
     $scope.popup_show_active = boolean;
     $scope.selected_movie = movie;
+    $scope.length_item = 3;
     if(id == 0){
 
     } else {
       $http({
-      	method: 'GET', //방식
-      	url: 'http://eb-fingo-real.ap-northeast-2.elasticbeanstalk.com/api/v1.0/movie/detail/'+$scope.movie_id+'/', /* 통신할 URL */
-      	data: dataObject, /* 파라메터로 보낼 데이터 */
-      	headers: {'Authorization': 'Token cb4a980f063b2dd4b550886526494091c131448f'} //헤더
+         method: 'GET', //방식
+         url: 'http://fingo2-dev.ap-northeast-2.elasticbeanstalk.com/api/v1.0/movie/detail/'+$scope.movie_id+'/', /* 통신할 URL */
+         data: dataObject, /* 파라메터로 보낼 데이터 */
+         headers: {'Authorization': 'Token 0428140f0f353791520d51d20ce445c7d41c5cad'} //헤더
       })
       .success(function(data, status, headers, config) {
-      	if( data ) {
-      		/* 성공적으로 결과 데이터가 넘어 왔을 때 처리 */
+         if( data ) {
+            /* 성공적으로 결과 데이터가 넘어 왔을 때 처리 */
           // console.log(data.data[0].movie.title);
           $scope.fingo_movie_detail = data;
           console.log($scope.fingo_movie_detail);
-      	}
-      	else {
-      		/* 통신한 URL에서 데이터가 넘어오지 않았을 때 처리 */
+         }
+         else {
+            /* 통신한 URL에서 데이터가 넘어오지 않았을 때 처리 */
           console.log(error);
-      	}
+         }
       })
       $http({
         method: 'GET', //방식
-        url: 'http://eb-fingo-real.ap-northeast-2.elasticbeanstalk.com/api/v1.0/movie/detail/'+$scope.movie_id+'/comments/', /* 통신할 URL */
-        data: dataObject, /* 파라메터로 보낼 데이터 */
+        url: 'http://fingo2-dev.ap-northeast-2.elasticbeanstalk.com/api/v1.0/movie/detail/'+$scope.movie_id+'/comments/', /* 통신할 URL */
+        //http://fingo2-dev.ap-northeast-2.elasticbeanstalk.com/api/v1.0/movie/boxoffice/
         //http://eb-fingo-real.ap-northeast-2.elasticbeanstalk.com/api/v1.0/movie/detail/1/comments/
-        headers: {'Authorization': 'Token cb4a980f063b2dd4b550886526494091c131448f'} //헤더
+        data: dataObject, /* 파라메터로 보낼 데이터 */
+        headers: {'Authorization': 'Token 0428140f0f353791520d51d20ce445c7d41c5cad'} //헤더
       })
       .success(function(data, status, headers, config) {
-      	if( data ) {
+         if( data ) {
           $scope.fingo_movie_detail_comment = data;
           console.log($scope.fingo_movie_detail_comment);
-      	}
-      	else {
-      		/* 통신한 URL에서 데이터가 넘어오지 않았을 때 처리 */
+         }
+         else {
+            /* 통신한 URL에서 데이터가 넘어오지 않았을 때 처리 */
           console.log(error);
-      	}
+         }
       })
 
       .error(function(data, status, headers, config) {
-      	/* 서버와의 연결이 정상적이지 않을 때 처리 */
-      	console.log(status);
+         /* 서버와의 연결이 정상적이지 않을 때 처리 */
+         console.log(status);
       });
     }
 
