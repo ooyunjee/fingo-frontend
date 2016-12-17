d6bed7a848e99846515061eb6bc50955a97a003c
heoyunjee <heoyunjee@gmail.com>
2016-12-17 22:19:36 +0900

    Refactor main-page.js

--- src/js/main-page.js
+++ src/js/main-page.js
@@ -1,11 +1,13 @@
-var header_carousel = {
-  'width': 1640,
-  'height': {
-    'desktop': 850,
-    'tablet': 650,
-    'mobile': 600
-  }
-};
+var $ = require('jquery');
+
+// var header_carousel = {
+//   'width': 1640,
+//   'height': {
+//     'desktop': 850,
+//     'tablet': 650,
+//     'mobile': 600
+//   }
+// };
 // var main_box_office_carousel = {
 //   'height': {
 //     'desktop': 530,
@@ -59,7 +61,7 @@ var main_movie_trailers_carousel = {
 };
 
 
-$('#main-header-carousel').fingoCarousel(header_carousel);
+// $('#main-header-carousel').fingoCarousel(header_carousel);
 // $('#main-box-office-carousel').fingoCarousel(main_box_office_carousel);
 // $('#main-movie-ranking-carousel').fingoCarousel(main_movie_ranking_carousel);
 $('#main-new-movie-trailers-carousel').fingoCarousel(main_movie_trailers_carousel);
@@ -77,77 +79,86 @@ $('#main-new-movie-trailers-carousel').fingoCarousel(main_movie_trailers_carouse
 // var $ca = $('#carousel').fingoCarousel({});
 
 
-  var main_movie_ranking_btn_group = document.querySelector('.main-movie-ranking-btn-group');
-  console.log(main_movie_ranking_btn_group);
-  var active_btns = main_movie_ranking_btn_group.querySelectorAll('button');
-
-  active_btns.forEach(function(btn, index) {
-    btn.addEventListener('click', function() {
-      $(btn).radioClass('active');
-    });
-  });
-
-
+  // var $main_movie_ranking_btn_group = $('.main-movie-ranking-btn-group');
+  // console.log($main_movie_ranking_btn_group);
+  // var $active_btns = $main_movie_ranking_btn_group.find('button');
+  //
+  // $active_btns.each(function(btn, index) {
+  //   btn.addEventListener('click', function() {
+  //     $(btn).radioClass('active');
+  //   });
+  // });
 
 
-  var tabs = document.querySelectorAll('.ui-carousel-tab');
+
+
+  var $tabs = $('.ui-carousel-tab');
+  console.log($tabs);
 
   // 인디케이터 기본 이벤트 차단
-  tabs.forEach(function(tab) {
+  $tabs.each(function(tab) {
     tab.addEventListener('click', function(e) {
       if(e) { e.preventDefault(); }
     });
   });
 
-  var box_office_list = document.querySelector('.main-box-office-list');
-  var box_office_list_btn = box_office_list.querySelector('button');
-  var ul = box_office_list.querySelector('ul');
+  // var box_office_list = document.querySelector('.main-box-office-list');
+  // var box_office_list_btn = box_office_list.querySelector('button');
+  // var ul = box_office_list.querySelector('ul');
+  //
+  // var index = 1;
+  // var intervalID = null;
+  // var $ul = $(ul);
+  // var rolling_time = 1000;
+  //
+  //
+  // // Box Office List 버튼 이벤트 처리
+  // box_office_list_btn.addEventListener('click', function() {
+  //   box_office_list.classList.toggle('show');
+  //   if(box_office_list.classList.contains('show')) {
+  //     // clearInterval(intervalID);
+  //     $ul.css('top', 0);
+  //     for(var i = 0, l = index - 1; i < l; i++) {
+  //       $ul.prepend($ul.children().last());
+  //     }
+  //   } else {
+  //     index = 1;
+  //   }
+  // });
+  //
+  // // Box Office List 롤링 수행
+  // if(box_office_list.classList.contains('show')) {
+  //   clearInterval(intervalID);
+  // } else if(!box_office_list.classList.contains('show')) {
+  //   intervalID = window.setInterval(rollingList, rolling_time);
+  // }
+  //
+  // // Box Office List 롤링 함수
+  // function rollingList() {
+  //   var list_length = $ul.children().length;
+  //   var list_height = $ul.children().eq(0).height();
+  //
+  //   $ul.stop().animate({
+  //     'top': -list_height
+  //   }, 600, function() {
+  //     $(this).append($(this).children().first());
+  //     $(this).css('top', 0);
+  //     index++;
+  //   });
+  // }
+  //
+  // box_office_list.addEventListener('mouseover', function() {
+  //   clearInterval(intervalID);
+  // });
+  //
+  // box_office_list.addEventListener('mouseout', function() {
+  //   if(!box_office_list.classList.contains('show')) {
+  //     intervalID = window.setInterval(rollingList, rolling_time);
+  //   }
+  // });
 
-  var index = 1;
-  var intervalID = null;
-  var $ul = $(ul);
 
 
-  // Box Office List 버튼 이벤트 처리
-  box_office_list_btn.addEventListener('click', function() {
-    box_office_list.classList.toggle('show');
-    if(box_office_list.classList.contains('show')) {
-      $ul.css('top', 0);
-      clearInterval(intervalID);
-      for(var i = 0, l = index - 1; i < l; i++) {
-        $ul.prepend($ul.children().last());
-      }
-    } else if(!box_office_list.classList.contains('show')) {
-      index = 1;
-      intervalID = window.setInterval(rollingList, 2500);
-    }
-  });
 
-  // Box Office List 롤링 함수
-  function rollingList() {
-    var list_length = $ul.children().length;
-
-    $ul.stop().animate({
-      'top': -45
-    }, 600, function() {
-      $(this).append($(this).children().first());
-      $(this).css('top', 0);
-      index++;
-    });
-  }
-
-  // Box Office List 롤링 수행
-  if(!box_office_list.classList.contains('show')) {
-    intervalID = window.setInterval(rollingList, 2500);
-  }
-
-
-  box_office_list.addEventListener('mouseover', function() {
-    clearInterval(intervalID);
-  });
-
-  box_office_list.addEventListener('mouseout', function() {
-    intervalID = window.setInterval(rollingList, 2500);
-  });
 
 // })(this, this.jQuery);
